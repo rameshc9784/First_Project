2e2a441d9d009ae4f8e3c9e68e995964f2519187
Ramesh Choudhary <rameshc9784@gmail.com>
2025-01-21 15:25:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Ramesh, How are you?</h1>
+      <p>Let's coding</p>
+    </div>
+  )
+}
+
+export default App
